65dc5c36a04529bafe636c88b0e0b064283c1431
ajaypatel07 <matholiyaajay@gmail.com>
2024-01-28 12:25:16 -0800

    `-`-`

---
+++ def.c
@@ -0,0 +1,25 @@
+
+// Write a programe to print following pattern
+// 0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ... 10000
+#include <stdio.h>
+void main()
+{
+    int z = 0;
+    {
+        printf("%d\t",z);
+    }
+
+
+
+    int number1 = 1;
+    int a;
+
+    while (number1 <= 10000)
+    {
+        number1 = number1 + a;
+        a++;
+
+        printf("%d\t", number1);
+        number1 = number1 + 1;
+    }
+}
