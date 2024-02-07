f53279c1c9677d7caa49932e8b64ede862abf2ca
ajaypatel07 <matholiyaajay@gmail.com>
2024-02-06 16:58:57 -0800

    ---

---
+++ array4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void main()
+{
+    int a[5];
+    int i;
+    int j;
+    int temp;
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("enter number %d :",i);
+        scanf("%d", &a[i]);
+    }
+    for (i = 0; i < 5; i++)
+    {
+        for (j = i + 1; j < 5; j++)
+        {
+            if (a[i] > a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+        printf(" Array element: ");
+        for(i=0;i<5;i++)
+        {
+            printf("%d",a[i]);
+
+        }
+
+}
